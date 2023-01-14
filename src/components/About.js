d8f7fd897343e53d42b1b2b1eb49271e9fba07c2
James Blaney <jamesblaney93@gmail.com>
2023-01-14 14:45:44 +0000

    homepage and about page routed and working

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div>
+            <h1>ABOUT PAGE</h1>
+            <p>Welcome to about page</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>HOMEPAGE</h1>
+            <p>Welcome to homepage!</p>
+        </div>
+    )
+}
+
+export default Home;
