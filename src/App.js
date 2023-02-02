7e80615af27c31e36e714acb0f4a22f3545f470c
James Blaney <jamesblaney93@gmail.com>
2023-02-02 15:56:14 +0000

    configuring custom domain

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import MainContainer from './containers/MainContainer';
-// import TravelTrackerCountries from './images/TravelTrackerCountries';
 
 function App() {
   return (
